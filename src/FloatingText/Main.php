4c694918d79846a3a097d46a147717bd849bd937
MagicPanda <MagicPandaDev@users.noreply.github.com>
2016-11-26 06:33:32 -0700

    Fix

--- src/FloatingText/Main.php
+++ src/FloatingText/Main.php
@@ -43,14 +43,13 @@ class Main extends PluginBase implements Listener{
                 $line3 = $cfg->get("LINE3"); 
                 $line4 = $cfg->get("LINE4"); 
                 $line5 = $cfg->get("LINE5"); 
-                      $online = count(Server::getInstance()->getOnlinePlayers()); 
-                     $maxonline = $this->getServer()->getMaxPlayers();
-                    $playername = $player->getName();
-                   $ip = $this->getServer()->getIp();
-                  $mote = $this->getServer()->getMotd();
-                 $port = $this->getServer()->getPort();
-                $version = $this->getServer()->getVersion();     
-               $health = $this->player()->getHealth();                                     
+                     $online = count(Server::getInstance()->getOnlinePlayers()); 
+                    $maxonline = $this->getServer()->getMaxPlayers();
+                   $playername = $player->getName();
+                  $ip = $this->getServer()->getIp();
+                 $mote = $this->getServer()->getMotd();
+                $port = $this->getServer()->getPort();
+               $version = $this->getServer()->getVersion();                                  
               $rs = TF::RESET. "\n";
               $allline = $line1. $rs. $line2. $rs. $line3. $rs. $line4. $rs. $line5; 
               $allline = str_replace("{ONLINE}", $online, $allline);
@@ -60,7 +59,6 @@ class Main extends PluginBase implements Listener{
               $allline = str_replace("{MOTE}", $mote, $allline);
               $allline = str_replace("{PORT}", $port, $allline);
               $allline = str_replace("{VERSION}", $version, $allline);
-              $allline = str_replace("{HEALTH}", $Health, $allline);
               $level->addparticle(new FloatingTextParticle(new Vector3($x1, $y1, $z1), $allline));
             
          }
